7d9fede5dccc8a0cfe3574e74aefb213a9cadb1e
dccupp <dccupp@gmail.com>
2015-11-09 14:36:11 -0500

    create, animate,  and remove drops

--- js/item.js
+++ js/item.js
@@ -2,11 +2,15 @@
  * The Item class is a blueprint for each game element we generate
  * @author  John Doe
  * @version 1.0, Nov 2015
+ *
+ * @param cl    which class to use to style it
+ * @param xpos  int - horizontal starting position
+ * @param ypos  int - vertical starting position
  */
-function Item(){
+function Item(cl, xpos, ypos){
 	//properties to let each item store its own x/y position
-	this.x = Math.floor(Math.random()*720); 
-	this.y = Math.floor(Math.random()*500);
+	this.x = xpos;
+	this.y = ypos;
 	//property that keeps track of the item's visual
 	this.item_on_page;
 	/**
@@ -14,7 +18,7 @@ function Item(){
 	 */ 
 	 this.create = function(){
 		 this.item_on_page = document.createElement("div");
-		 this.item_on_page.className = "raindrop";
+		 this.item_on_page.className = cl;
 		 //use this object's x & y var's to position it in CSS
 		 this.item_on_page.style.left = this.x + "px";
 		 this.item_on_page.style.top = this.y + "px";
@@ -25,7 +29,21 @@ function Item(){
 	 * The destroy method removes the item from the game
 	 */ 
 	 this.destroy = function(){
-		 
+         //remove all splashes from the screen
+         var splashes = doucment.getElementsByClassName("splash");
+         //assign as animating GIF where the drop is
+         var newsplash = document.createElement("img");
+         newsplash.src = "img/splash.gif?"+Math.random();
+         newsplash.style.position = "absolute";
+         newsplash.style.left = this.x+"px";
+         newsplash.style.top = this.y+"px";
+         document.body.appendChild(newsplash);
+         //figure out this drop's index num
+         var index_num = drop_array.indexOf(this);
+         //to remove the item from the array: you use splice(index,howMany);
+         drop_array.splice(index_num,1);
+         //remove div as a child of the body
+         document.body.removeChild(this.item_on_page);
 	 }//end function destroy()
 
 }//end the Item class
